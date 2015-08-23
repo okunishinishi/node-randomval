9d5ac93f9ddb3320348221f16765d2d84b651a9a
Taka Okunishi <okunishinishi@gmail.com>
2015-08-23 17:32:27 +0900

    Add travis settings

---
+++ test/randomval_test.js
@@ -0,0 +1,20 @@
+/**
+ * Test case for randomval.
+ * Runs with nodeunit.
+ */
+
+var randomval = require('../lib/randomval.js');
+
+exports.setUp = function(done) {
+    done();
+};
+
+exports.tearDown = function(done) {
+    done();
+};
+
+exports['Randomval'] = function(test){
+
+    test.done();
+};
+
